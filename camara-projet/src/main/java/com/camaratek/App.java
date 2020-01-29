78ac3773bd9a21ab5a70539aa51b34cf5ab42fa2
camara94 <ldamaro98@gmail.com>
2020-01-29 21:58:46 +0100

    Création de projet maven avec invite mvn

---
+++ camara-projet/src/main/java/com/camaratek/App.java
@@ -0,0 +1,13 @@
+package com.camaratek;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Bonjour tout le monde " );
+    }
+}
